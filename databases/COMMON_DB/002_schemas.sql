8e8a335f593daf1e2148a90f831fe90fdadb5aea
Raviteja Potluru <36457122+raviteja0012@users.noreply.github.com>
2025-11-21 11:39:24 -0600

    Create CSV_FORMAT file format in FILE_FORMATS schema
    Added a CSV file format definition under FILE_FORMATS schema.

--- databases/COMMON_DB/002_schemas.sql
+++ databases/COMMON_DB/002_schemas.sql
@@ -4,5 +4,10 @@ USE DATABASE COMMON_DB;
 CREATE SCHEMA IF NOT EXISTS FILE_FORMATS
     COMMENT = 'Shared file format definitions';
 
-CREATE SCHEMA IF NOT EXISTS STAGES
-    COMMENT = 'External and internal stages';
+-- Create a sample file format
+CREATE OR REPLACE FILE FORMAT FILE_FORMATS.CSV_FORMAT
+    TYPE = 'CSV'
+    FIELD_DELIMITER = ','
+    SKIP_HEADER = 1
+    NULL_IF = ('NULL', 'null')
+    EMPTY_FIELD_AS_NULL = TRUE;
